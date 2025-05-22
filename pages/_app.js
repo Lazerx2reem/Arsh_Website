b0c423d2e9421029a9b95a0be5f5eda45ba8a542
Arsh Gupta <arsh121523@gmail.com>
2025-05-22 19:12:32 +0530

    Updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'
+import { Inter } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const inter = Inter({ subsets: ['latin'] })
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={inter.className}>
+      <ThemeProvider attribute="class" enableSystem={true}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </main>
+  )
 }
